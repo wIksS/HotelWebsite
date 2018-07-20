b51c5b01bcd55d19a999c4b60000f4595ba24ed5
Viktor Dakov <viktordakov@Viktors-MacBook-Pro.local>
2018-07-20 04:08:59 +0300

    Fixed react bug not working on different pages

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import registerServiceWorker from './registerServiceWorker';
 import {fetchHotelDescription} from './actions/hotelDescription'
 import {fetchHotelOverview} from './actions/hotelOverview'
 import {Provider} from 'react-redux'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, HashRouter} from 'react-router-dom'
 import HotelDescriptionAdminWrapper from "./containers/HotelDescriptionAdminWrapper";
 import HotelDescriptionWrapper from "./containers/HotelDescriptionWrapper";
 import HotelOverviewAdminWrapper from "./containers/HotelOverviewAdminWrapper";
@@ -25,41 +25,44 @@ const store = createStore(app,
 store.dispatch(fetchHotelDescription());
 store.dispatch(fetchHotelOverview());
 
-render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route path='/administration' component={HotelDescriptionAdminWrapper}/>
-                <Route path='/' component={HotelDescriptionWrapper}/>
-            </Switch>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('hotel-description')
-)
+if (document.getElementById('hotel-description')) {
+    render(
+        <Provider store={store}>
+            <HashRouter>
+                <Switch>
+                    <Route path='/administration' component={HotelDescriptionAdminWrapper}/>
+                    <Route path='/' component={HotelDescriptionWrapper}/>
+                </Switch>
+            </HashRouter>
+        </Provider>,
+        document.getElementById('hotel-description')
+    )
+}
 
-
-render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route path='/administration' component={HotelOverviewAdminWrapper}/>
-                <Route path='/' component={HotelOverviewWrapper}/>
-            </Switch>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('hotel-overview')
-)
+if (document.getElementById('hotel-overview')) {
+    render(
+        <Provider store={store}>
+            <HashRouter>
+                <Switch>
+                    <Route path='/administration' component={HotelOverviewAdminWrapper}/>
+                    <Route path='/' component={HotelOverviewWrapper}/>
+                </Switch>
+            </HashRouter>
+        </Provider>,
+        document.getElementById('hotel-overview')
+    )
+}
 
 
 if (document.getElementById('about-hotel')) {
     render(
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <Switch>
                     <Route path='/administration' component={HotelOverviewAdminWrapper}/>
-                    <Route component={HotelOverviewWrapper}/>
+                    <Route path='/' component={HotelOverviewWrapper}/>
                 </Switch>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>,
         document.getElementById('about-hotel')
     )
