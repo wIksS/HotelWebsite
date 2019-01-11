c739f37fb7b18ca16e669d95bf6d1a2ca1915a65
Viktor Dakov <viktordakov97@gmail.com>
2019-01-12 00:32:51 +0200

    Changed server URL

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,3 +1,3 @@
 export const HotelOverviewText = 'ЗА ХОТЕЛА';
-export const BaseUrl = "http://localhost:8080";
-//export const BaseUrl = "http://185.80.1.67:8080";
+//export const BaseUrl = "http://localhost:8080";
+export const BaseUrl = "http://185.80.1.67:8080";
